23cde51c0d365ce78066c4869dd9881e74c4560e
Rumazor <th3rum2084@gmail.com>
2022-04-04 17:06:12 -0500

    "Light Mode - Dark Mode texto"

--- darkmode.js
+++ darkmode.js
@@ -1,9 +1,15 @@
 const btn = document.querySelector('.btn');
-
+btn.innerHTML = 'Dark Mode'
 
 btn.addEventListener('click', darkmode)
     
 function darkmode(){
     document.documentElement.classList.toggle('dark-theme')
     btn.classList.toggle('btn-dark')
+
+    if(btn.innerHTML === 'Dark Mode'){
+        btn.innerHTML = 'Light Mode'
+    } else if(btn.innerHTML === 'Light Mode'){
+        btn.innerHTML = 'Dark Mode'
+    }
 }
